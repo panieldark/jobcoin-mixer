1d4153dd7d74ac2092a224500abd9539cfac22aa
danielpark <danielpark1140@gmail.com>
2021-07-25 23:23:53 -0400

    Remove WIP function

--- djangomixer/djangomixer/settings.py
+++ djangomixer/djangomixer/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-from corsheaders.defaults import default_headers
+# from corsheaders.defaults import default_headers
 
 from dotenv import load_dotenv
 
