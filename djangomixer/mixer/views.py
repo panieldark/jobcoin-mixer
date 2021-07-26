59516a9ff09e4ebbbee8a8ae216020a27b64df8d
danielpark <danielpark1140@gmail.com>
2021-07-26 16:53:48 -0400

    Final views.py submission

--- djangomixer/mixer/views.py
+++ djangomixer/mixer/views.py
@@ -30,11 +30,10 @@ def create_request(request):
     dest3 = request.POST.get('dest3')
     dest4 = request.POST.get('dest4')
     dest5 = request.POST.get('dest5')
-    print(src, dest1)
     
     deposit_address = uuid.uuid4().hex
-    new_req = MixerRequest.objects.create(src_address=src, dest_address = dest1, dest_address_2=dest2, dest_address_3=dest3, dest_address_4 = dest4, dest_address_5 = dest5, deposit_address = deposit_address, status = 'created')
-    print(new_req)
+    new_req = MixerRequest.objects.create(src_address=src, dest_address = dest1, dest_address_2=dest2, \
+        dest_address_3=dest3, dest_address_4 = dest4, dest_address_5 = dest5, deposit_address = deposit_address, status = 'created')
     context['deposit_address'] = deposit_address
     return render(request, "mixer/request_created.html", context)
 
@@ -51,11 +50,10 @@ def create_request_ajax(request):
     dest3 = request.POST.get('dest3')
     dest4 = request.POST.get('dest4')
     dest5 = request.POST.get('dest5')
-    print(src, dest1)
     
     deposit_address = uuid.uuid4().hex
-    new_req = MixerRequest.objects.create(src_address=src, dest_address = dest1, dest_address_2=dest2, dest_address_3=dest3, dest_address_4 = dest4, dest_address_5 = dest5, deposit_address = deposit_address, status = 'created')
-    print(new_req)
+    new_req = MixerRequest.objects.create(src_address=src, dest_address = dest1, dest_address_2=dest2, \
+        dest_address_3=dest3, dest_address_4 = dest4, dest_address_5 = dest5, deposit_address = deposit_address, status = 'created')
     context['deposit_address'] = deposit_address
 
     if new_req:
@@ -68,6 +66,9 @@ API_BASE_URL = 'https://jobcoin.gemini.com/kept-velvet/api'
 API_TRANSACTIONS_URL = '{}/transactions'.format(API_BASE_URL)
 
 
+
+# API endpoint that will process all mixer requests. Is hit by a scheduled Lambda function, 
+# it will process any open mixer request that is found in the recent transactions list
 
 @csrf_exempt
 def open_requests_api(request):
@@ -89,37 +90,44 @@ def open_requests_api(request):
 
         # Grab only the new transactions, as indicated by cached count
         new_transactions = transactions_json[-new_count:]
-        print(new_transactions)
 
         # Make a dict for O(1) lookups
         req_map = dict()
         for req in open_requests:
+
+            # Future improvement: Make the key more unique-proof
             req_map[f'{req.src_address}|{req.deposit_address}'] = req
         
 
-
-        print("Requests dict:", req_map)
+        # Iterate the recent transactions, check to see if any match to an open request.
         for transaction in new_transactions:
-            print("Transaction:", transaction)
             
             # Edge case: handle new money (no fromAddress)
             if 'fromAddress' not in transaction:
                 continue
             
+            
             trans_string = f"{transaction['fromAddress']}|{transaction['toAddress']}"
             
             if trans_string in req_map.keys():
-                print('in keys', trans_string)
                 amount = float(transaction['amount'])
 
                 
                 deposit_address = req_map[trans_string].deposit_address
-                final_address = req_map[trans_string].dest_address
+
+                # compile list of destination wallets
+                destinations_list = [address for address in [req_map[trans_string].dest_address, req_map[trans_string].dest_address_2, \
+                    req_map[trans_string].dest_address_3, req_map[trans_string].dest_address_4, req_map[trans_string].dest_address_5] if address]
 
                 # Take a 3% fee, add it to cryptomixer wallet, return new amount
                 new_amount = take_fee(deposit_address, amount)
 
-                mixed_request = mix_coins_amongst_wallets(deposit_address, final_address, new_amount)
+                # Deliver to each destination address a fraction of the new amount
+                for dest in destinations_list:
+                    mixed_request = mix_coins_amongst_wallets(deposit_address, dest, new_amount/len(destinations_list))
+                    if not mixed_request:
+                        return HttpResponse('Failed to deliver')
+
                 
                 
                 # Update the request object
@@ -156,14 +164,12 @@ def mix_coins_amongst_wallets(depositedAddress, final_address, totalAmount):
         # Floor to 2 decimal places
         current_amount = math.floor(random_percent*totalAmount)/100
 
-        print("\nRandom percent:", random_percent, '%')
-        print("Amount this round: ${}".format(current_amount))
-        random_num_wallets = random.randint(2,5)
         
         previous_wallet = depositedAddress
 
 
         # choose a path of 2-5 wallets for random_percent*amount from start to finish
+        random_num_wallets = random.randint(2,5)
         for i in range(random_num_wallets):
             next_wallet = house_wallets[random.randint(3,7)]
             
@@ -171,17 +177,14 @@ def mix_coins_amongst_wallets(depositedAddress, final_address, totalAmount):
                 continue
             # make API calls to send money
             try:
-                print(f"{previous_wallet} -> {next_wallet}: ${current_amount}")
                 req = requests.post(API_TRANSACTIONS_URL, data={"fromAddress": previous_wallet, "toAddress": next_wallet, 'amount': current_amount})
                 if int(req.status_code) >= 400:
                     raise Exception(req.status_code, req.reason)
             except Exception as e:
-                print(e)
                 return False
             
             previous_wallet = next_wallet
         
-        print(f"FINAL: {previous_wallet} -> {final_address}: ${current_amount}")
         # finally, send this chunk of money to final destination
         req = requests.post(API_TRANSACTIONS_URL, data={"fromAddress": previous_wallet, "toAddress": final_address, 'amount': current_amount})
 
@@ -193,7 +196,6 @@ def mix_coins_amongst_wallets(depositedAddress, final_address, totalAmount):
 def take_fee(deposit_address, origAmount):
     # 3% fee, floored to 2 dec places
     fee = math.floor(3 * origAmount)/100
-    print(f"FEE: {deposit_address} -> {'CryptoMixer'}: ${fee}")
     req = requests.post(API_TRANSACTIONS_URL, data={"fromAddress": deposit_address, "toAddress": 'CryptoMixer', 'amount': fee})
     new_amount = origAmount - fee
     return new_amount
