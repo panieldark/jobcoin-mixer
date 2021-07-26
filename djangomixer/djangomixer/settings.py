352b7609c04b6fa99977f98ac43a9617c17dc213
panieldark <danielpark1140@gmail.com>
2021-07-26 04:15:51 +0000

    AWS commit

--- djangomixer/djangomixer/settings.py
+++ djangomixer/djangomixer/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-1xar)$bp$v9z1yxx%l$yjwqo11jf@o_&&c22lngr)byzad)3=c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
